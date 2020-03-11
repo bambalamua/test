a570642b5d82ff253be062b1279cbac14bf1eadf
Bohdan <bambalamua@gmail.com>
2020-03-11 14:08:19 +0200

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+withdraw = int(input("Value: "))
+banknotes = {5: 100, 10: 40, 50: 800}
+bank = 0
+
+for value in banknotes:
+    bank += value * banknotes[value]
+
+if withdraw > bank:
+    print('There is not enough cash in the ATM')
+elif withdraw % 5 != 0:
+    print('The value must be a multiple of 5')
+else:
+    cash = {}
+    for value in list(banknotes.keys())[::-1]:
+        if withdraw < value*banknotes[value] and withdraw // value != 0:
+            cash[value] = withdraw // value
+            withdraw -= value * (withdraw // value)
+    print(cash)
